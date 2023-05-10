b249b07b1b9702a5f10f105553cf64ad0fe30d58
JBrown413 <131727345+JBrown413@users.noreply.github.com>
2023-05-10 15:12:40 -0400

    Reverse solution seems to run properly but haven't verified results yet

--- loran_js/plain_js/loranConversionApp.js
+++ loran_js/plain_js/loranConversionApp.js
@@ -1,6 +1,6 @@
 var N1, N2, N3, N4, N5, N6, N7, N8;
 var N9=15; // N9=NO. OF MASTER STATIONS
-var flatteningFactor=1/298.26;
+var flatteningFactor = C0 =1/298.26;
 var A0=-1;
 var PI=4.*Math.atan(1);
 var TP=PI+PI;
@@ -20,22 +20,22 @@ var I9$;
 var N0;
 var P0;
 var L0;
-var W$ = [];
+var W$ = {};
 var D9 = [];
-var P1_arr = [];
-var L1_arr = [];
+var P1_arr = {};
+var L1_arr = {};
 var G1$;
 var G2$;
-var D_arr = [];
+var D_arr = {};
 var X;
 var S;
 var H;
 var M0;
 var V;
-var P_arr = [];
-var L_arr = [];
+var P_arr = {};
+var L_arr = {};
 var A;
-var A_arr = [];
+var A_arr = {};
 var A1;
 var B1;
 var C1;
@@ -48,6 +48,7 @@ var K;
 var R;
 var YY;
 var XX;
+var ZZ=0;
 var G;
 var Z;
 var S0;
@@ -79,7 +80,8 @@ var F2=1;
 var ITD;
 var GRI;
 var T;
-var T_arr = [];
+var T_arr = {};
+var C0;
 
 function ItdValidContinue()
 {
@@ -92,43 +94,80 @@ function test()
 	A = 1;
 }
 
+// was basic subroutine 1270
+function buildString()
+{
+	
+}
+
 function convertLoranToLL(GRI, loran1, loran2)
 {
 	I9$ = GRI;
 	F0 = 1;
 	
-	var slave1 = findSlave(GRI, loran1);
-	var slave2 = findSlave(GRI, loran2);
-	console.log(slave1, slave2);
-	G$.push(GRI + slave1, GRI + slave2);
+	// find sub (slave) towers 
+	slave1 = findSlave(GRI, loran1);
+	slave2 = findSlave(GRI, loran2);
+	
+	// debug output 
+	console.log(slave1, slave2);	
 	console.log(G$);
 
-	var towersObj = getTowerData();
+	// get coords and delays for all towers in this GRI triad
+	towersObj = getTowerData();
 	P0 = towersObj[GRI].lat;
 	L0 = towersObj[GRI].long;
 
+	// assign values to someglobal iable
+	G1$ = GRI + slave1;
+	G2$ = GRI + slave2;
+	G$.push(G1$, G2$);
 	D9.push(towersObj[GRI][slave1]['delay']);
 	P1 = towersObj[GRI][slave1]['lat'];
 	L1 = towersObj[GRI][slave1]['long'];
-
 	D9.push(towersObj[GRI][slave2]['delay']);
 	P2 = towersObj[GRI][slave2]['lat'];
 	L2 = towersObj[GRI][slave2]['long'];
 
-	console.log(towersObj[GRI][slave1]);
-	console.log(towersObj[GRI][slave2]);
+	// debug output
+	//console.log(towersObj[GRI][slave1]);
+	//console.log(towersObj[GRI][slave2]);
+	
+	X=P0;
+	calculateV();
+	X=V*RD;
+	getAtan();
+	P_arr[0] = X;
+	X = L0;
+	calculateV();
+	L_arr[0] = V*RD;
+	
+	X=P1;
+	calculateV();
+	X=V*RD;
+	getAtan();
+	P_arr[1] = X;
+	X=L1;
+	calculateV();
+	L_arr[1] = V*RD;
+	console.log(P1)
+	console.log(X, L_arr[0]);
+	console.log(P_arr[0]);
+	console.log(L_arr[1]);
+	console.log( P_arr[1]);
+	reverseSolution();
 }
 
 //FIXING ROUTINE - was basic subroutine 770
 function fixingRoutine()
 {
-	var A1=F1*MATH.sin(ITD[0]);
-	var B1=MATH.cos(ITD[0])-MATH.cos(B[0]);
-	var C1=MATH.sin(B[0]);
-	var A2=F2*MATH.sin(ITD[1]);
-	var B2=MATH.cos(ITD[1])-MATH.cos(B[1]);
-	var C2=MATH.sin(B[1]);
-	var E1=Z[0][0];
+	 A1=F1*MATH.sin(ITD[0]);
+	 B1=MATH.cos(ITD[0])-MATH.cos(B[0]);
+	 C1=MATH.sin(B[0]);
+	 A2=F2*MATH.sin(ITD[1]);
+	 B2=MATH.cos(ITD[1])-MATH.cos(B[1]);
+	 C2=MATH.sin(B[1]);
+	 E1=Z[0][0];
 	
 	if(F1 == -1) 
 	{
@@ -142,35 +181,35 @@ function fixingRoutine()
 		E2=Z[1][1];
 	}
 	
-	var C=B1*C2*MATH.cos(E2)-B2*C1*MATH.cos(E1);
-	var S=B1*C2*MATH.sin(E2)-B2*C1*MATH.sin(E1);
-	var K=B2*A1-B1*A2;
-	var R=SQR(C*C+S*S);
-	var YY=S;
-	var XX=C;
-	var P1;
+	 C=B1*C2*MATH.cos(E2)-B2*C1*MATH.cos(E1);
+	 S=B1*C2*MATH.sin(E2)-B2*C1*MATH.sin(E1);
+	 K=B2*A1-B1*A2;
+	 R=SQR(C*C+S*S);
+	 YY=S;
+	 XX=C;
+	 P1;
 	
-	var AN = getQatn(XX, YY);
+	 AN = getQatn(XX, YY);
 
-	var G=AN;
+	 G=AN;
 	XX=K/R;
 	
 	AN = getAcos(XX);
 	
-	var Z=G+A0*AN;
+	Z=G+A0*AN;
 	YY=B2;
 	XX=C2*MATH.cos(Z-E2)+A2;
 	
 	AN=getQatn(XX, YY);
 
-	var S0=AN;
+	 S0=AN;
 	
 	if (F2==1)
 	{
 		P1=P[0][1];
 	} 
 	
-	var L1=L[0][1];
+	 L1=L[0][1];
 
 	if (F2==-1)
 	{
@@ -179,29 +218,29 @@ function fixingRoutine()
 
 	L1=L[1][1];
 
-	var Z1=Z;
+	 Z1=Z;
 	
-	var directVals = directSolution();
+	 directVals = directSolution();
 	
 	// get from directSolution(): P2, L2 
-	var P0=directVals.P2;
-	var L0=directVals.L2;
-	var P=Math.atan(Math.tan(P0)/(1-flatteningFactor));
+	 P0=directVals.P2;
+	 L0=directVals.L2;
+	 P=Math.atan(Math.tan(P0)/(1-flatteningFactor));
 
 	P=P/RD;
-	var X=L0/RD;
-	var M0=360;
+	 X=L0/RD;
+	 M0=360;
 	
 	//GOSUB 1250: The BASIC program implies that we must call mConvert() and then getQatn() 
 	// but I believe it's unnecessary to call getQatn() here
 	X = mConvert(M0, X);
 	
-	var L=X;
+	 L=X;
 	//IF L>180 THEN L=L-360
 	L = L>180 ? L-360 : L;
 	
 	X=P;
-	var C$ = xConvert(X);
+	 C$ = xConvert(X);
 	P$=C$;
 
 	X=L;
@@ -211,37 +250,66 @@ function fixingRoutine()
 }
 
 // was basic subroutine 1260
-function getQatn(XX, YY)
+function getQatn()
 {
-	var AN=Math.atan(YY/(XX-1e-9*(XX=0)))-Math.PI*(XX<0);
+	AN=Math.atan(YY/(XX-1e-9*(XX=0)))-Math.PI*(XX<0);
 	return AN;
 }
 
 // was basic subroutine 1350
-function getAcos(XX)
+function getAcos()
 {
-	var AN=Math.atan(Math.sqrt(1-XX*XX)/(XX-1e-9*(XX=0)))-Math.PI*(XX<0);
+	AN=Math.atan(Math.sqrt(1-XX*XX)/(XX-1e-9*(XX=0)))-Math.PI*(XX<0);
 	return AN;
 }
 
-function getAsin(XX)
+function getAsin()
 {
-	var ZZ=Math.sqrt(1-XX*XX);
-	var AN=ATN(XX/(ZZ-1e-9*(ZZ=0)));
+	ZZ=Math.sqrt(1-XX*XX);
+	console.log(L)
+
+	AN=Math.atan(XX/(ZZ-1e-9*(ZZ==0)));
+	console.log(ZZ)
 	return AN;
+}
+
+// was basic subroutine 1270
+function getAtan()
+{
+	AN=Math.atan(YY/(XX-1e-9*(XX==0)))-PI*(XX<0);
 }
 
 // was basic subroutine 1250
 function mConvert(M0, X)
 {
-	X=X-M0*INT(X/M0);
+	X=X-M0*parseInt(X/M0);
 	return X;
+}
+
+// was basic subroutine 1360
+function calculateV()
+{
+	S = Math.sign(X);
+	X = Math.abs(X);
+	H = parseInt(X);
+	M0 = 1;
+	modFunction();
+	V=X*100;
+	X=V;
+	modFunction();
+	V=S*((100*X/60+parseInt(V))/60+H);
+
+}
+
+function modFunction()
+{
+	X=X-M0*parseInt(X/M0);
 }
 
 // was basic subroutine 1280
 function xConvert(X)
 {
-	var C$=" ";
+	 C$=" ";
 
 	if (X<0)
 	{
@@ -250,7 +318,7 @@ function xConvert(X)
 	}
 
 	X=X+1/7200;
-	var X0=parseInt(X);
+	X0=parseInt(X);
 	C$=C$+String(X0)+" ";
 	X=60*(X-X0);
 	X0=parseInt(X);
@@ -267,122 +335,124 @@ function xConvert(X)
 // Direct Solution - was basic subroutine 550
 function directSolution()
 {
-	var Z8=Math.sin(Z1);
-	var Z9=Math.cos(Z1);
-	var P8=Math.sin(P1);
-	var P9=Math.cos(P1);
-	var M=-Z8*P9;
-	var C1=flatteningFactor*M;
-	var C2=flatteningFactor*(1-M*M)/4;
-	var D=(1-C2)*(1-C2-C1*M);
-	var P=C2*(1+C1*M/2)/D;
-	var N=P9*Z9;
-	var YY=N;
-	var XX=P8;
+	 Z8=Math.sin(Z1);
+	 Z9=Math.cos(Z1);
+	 P8=Math.sin(P1);
+	 P9=Math.cos(P1);
+	 M=-Z8*P9;
+	 C1=flatteningFactor*M;
+	 C2=flatteningFactor*(1-M*M)/4;
+	 D=(1-C2)*(1-C2-C1*M);
+	 P=C2*(1+C1*M/2)/D;
+	 N=P9*Z9;
+	 YY=N;
+	 XX=P8;
 	
-	var AN = getQatn(XX, YY);
+	 AN = getQatn(XX, YY);
 	
-	var S1=AN;
-	var D0=S0/D;
-	var U=2*(S1-D0);
-	var W=1-2*P*Math.cos(U);
-	var V=COS(U+D0);
+	 S1=AN;
+	 D0=S0/D;
+	 U=2*(S1-D0);
+	 W=1-2*P*Math.cos(U);
+	 V=COS(U+D0);
 
-	var X=C2*C2*Math.sin(D0)*Math.cos(D0*(2*V*V-1));
-	var Y=2*P*V*W*Math.sin(D0);
-  	var S2=D0+X-Y;
-	var S8=Math.sin(S2);
-	var S9=Math.cos(S2);
-	var K=Math.sqrt(M*M+(N*S9-P8*S8)^2);
-  	var P2=Math.atan((P8*S9+N*S8)/K);
+	 X=C2*C2*Math.sin(D0)*Math.cos(D0*(2*V*V-1));
+	 Y=2*P*V*W*Math.sin(D0);
+  	 S2=D0+X-Y;
+	 S8=Math.sin(S2);
+	 S9=Math.cos(S2);
+	 K=Math.sqrt(M*M+(N*S9-P8*S8)^2);
+  	 P2=Math.atan((P8*S9+N*S8)/K);
   	YY=-S8*Z8;
 	XX=P9*S9-P8*S8*Z9;
 	
 	AN = getQatn(XX, YY);
 	S3=AN;
-  	var H=C1*(1-C2)*S2-C1*C2*S8*Math.cos(S1+S1-S2);
-	var L2=L1+S3-H;
+  	 H=C1*(1-C2)*S2-C1*C2*S8*Math.cos(S1+S1-S2);
+	 L2=L1+S3-H;
   	YY=-M;
 	XX=-(N*S9-P8*S8);
 	
 	AN = getQatn(XX, YY);
 	Z2=AN; // set global Z2 value
 
-	var ret = {P2, L2};
+	 ret = {P2, L2};
 	return ret;
 }
 
 // reverse solution - was basic subroutine 650
 function reverseSolution()
 {
-	var L3=L2-L1;
-	var P3=(P2-P1)/2;
-	var P4=(P1+P2)/2;
-	var P6=Math.sin(P3);
-	var P7=Math.cos(P3);
-	var P8=Math.sin(P4);
-	var P9=Math.cos(P4);
+	L3=L_arr[1]-L_arr[0];
+	P3=(P_arr[1]-P_arr[0])/2;
+	P4=(P_arr[0]+P_arr[1])/2;
+	P6=Math.sin(P3);
+	P7=Math.cos(P3);
+	P8=Math.sin(P4);
+	P9=Math.cos(P4);
+	console.log(P6);
+	H=P7*P7-P8*P8;
+	let p6sq = P6*P6;
+	L=P6*P6+H*Math.sin(L3/2)**2;
+	XX=Math.sqrt(L);
+	getAsin();
+	console.log(L);
+	D0=2*AN;
+	U=2*P8*P8*P7*P7/(1-L);
+	V=2*P6*P6*P9*P9/L;
+	X=U+V;
+	Y=U-V;
+	T=D0/Math.sin(D0);
+	D=4*T*T;
 
-	var H=P7*P7-P8*P8;
-	var L=P6*P6+H*SIN(L3/2)^2;
-	var XX=SQR(L);
+	E=2*Math.cos(D0);
+	A=D*E;
+	C=T-(A-E)/2;
+	N1=X*(A+C*X);
+	B=D+D;
+	N2=Y*(B+E*Y);
+	N3=D*X*Y;
 
-	var AN = getAsin(XX);
-	var D0=2*AN;
-	var U=2*P8*P8*P7*P7/(1-L);
-	var V=2*P6*P6*P9*P9/L;
-	var X=U+V;
-	var Y=U-V;
-	var T=D0/SIN(D0);
-	var D=4*T*T;
+	D2=flatteningFactor*flatteningFactor*(N1-N2+N3)/64;
+	D1=flatteningFactor*(T*X-Y)/4;
+	S0=(T-D1+D2)*Math.sin(D0);
 
-	var E=2*Math.cos(D0);
-	var A=D*E;
-	var C=T-(A-E)/2;
-	var N1=X*(A+C*X);
-	var B=D+D;
-	var N2=Y*(B+E*Y);
-	var N3=D*X*Y;
+	M=32*T-(20*T-A)*X-(B+4)*Y;
+	F=Y+Y-E*(4-X);
+	G=flatteningFactor*(T/2+flatteningFactor*M/64);
+	Q=-F*G*Math.tan(L3)/4;
 
-	var D2=flatteningFactor*flatteningFactor*(N1-N2+N3)/64;
-	var D1=flatteningFactor*(T*X-Y)/4;
-	var S0=(T-D1+D2)*Math.sin(D0);
+	L4=(L3+Q)/2;
+	L8=Math.sin(L4);
+	L9=Math.cos(L4);
 
-	var M=32*T-(20*T-A)*X-(B+4)*Y;
-	var F=Y+Y-E*(4-X);
-	var G=flatteningFactor*(T/2+flatteningFactor*M/64);
-	var Q=-F*G*Math.tan(L3)/4;
-
-	var L4=(L3+Q)/2;
-	var L8=Math.sin(L4);
-	var L9=Math.cos(L4);
-
-	var YY=P6*L9;
-	var XX=P9*L8;
-	var AN = getQatn(XX, YY);
-	var T1=AN;
+	YY=P6*L9;
+	XX=P9*L8;
+	AN = getQatn(XX, YY);
+	T1=AN;
 	YY=-P7*L9;
 	XX=P8*L8;
 	AN = getQatn(XX, YY);
 	T2=AN;
 
-	var M0=TP;
+	M0=TP;
 	X=T1+T2;
 	X = mConvert(M0, X);
-	var Z1=X;
+	Z1=X;
 	X=T1-T2;
 	X = mConvert(M0, X);
-	var Z2=X;
+	Z2=X;
 
-	var ret = {S0, Z1, Z2, A};
-	return ret;
+	//let ret = {S0, Z1, Z2, A};
+	//console.log(ret);
+	//return ret;
 
 }
 
 function getTowerData()
 {
-	var ret = {};
-	var towerData = {
+	let ret = {};
+	 towerData = {
 		"5930": {
 			"lat": 46.4827199,
 			"long": 67.5537713,
@@ -457,7 +527,7 @@ function getTowerData()
 */
 function findSlave(GRI, loranDelay)
 {
-	var ret = "";
+	let ret = "";
 
 	if (GRI == "9960") {
 		if (loranDelay >= 11000 && loranDelay < 25000) 
@@ -502,8 +572,8 @@ function findSlave(GRI, loranDelay)
 	return ret;
 }
 
-var GRI = '9960';
-var loran1 = 26600;
-var loran2 = 41400;
+ GRI = '9960';
+ var loran1 = 26600;
+ var loran2 = 41400;
 
 convertLoranToLL(GRI, loran1, loran2);
